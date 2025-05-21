dbfdda5b1eea1ee188bc11c543e52051b257f568
FanCheong <bryancwxuan@gmail.com>
2025-05-21 10:25:57 +0100

    Added some SEO and opengraph icon and names + changed tab icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,21 +14,26 @@
 	}
 </script>
 
+<svelte:head>
+  <meta property="og:title" content="Bryan Cheong | Portfolio" />
+  <meta property="og:image" content="https://bryancheong.dev/icon.jpeg" />
+</svelte:head>
+
 <Header y={y} />
 
 <div class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm
 sm:text-base min-h-screen">
-    <div class={"fixed bottom-10 right-10 duration-300 flex p-10 z-[10] " + (
-            y > 0 ? ' opacity-100 pointer-events-auto': ' pointer-events-none opacity-0'
-    )}>
+  <div class={"fixed bottom-10 right-10 duration-300 flex p-10 z-[10] " + (
+          y > 0 ? ' opacity-100 pointer-events-auto': ' pointer-events-none opacity-0'
+  )}>
     <button onclick={scrollToTop} 
             class="ml-auto bg-blue-900 hover:bg-blue-950 text-stone-200 p-3 rounded-full shadow-lg"
             aria-label="Scroll to top">
-                    <i class="fa-solid fa-arrow-up grid place-items-center aspect-square"></i>
-            </button>
-    </div>
-    {@render children()}
-    <Footer />
+        <i class="fa-solid fa-arrow-up grid place-items-center aspect-square"></i>
+    </button>
+  </div>
+  {@render children()}
+  <Footer />
 </div>
 
 <svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth />
